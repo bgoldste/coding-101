abfdbca66199f6c8bd834e5167664f5e82470c9c
Ben Goldstein <ben@Bens-MacBook-Pro.local>
2015-07-24 15:18:13 -0700

    changes to hello.py

--- coding-101/hello.py
+++ coding-101/hello.py
@@ -10,5 +10,70 @@
 #it is also common to 'comment out' code that you do not want to run anymore
 
 
-#our first code
-print "Hello, world!"
+
+#print hello world
+print "Hello, World"
+
+
+#Types
+
+# #string
+b = "hello"
+print b
+print type(b)
+
+
+# #int
+c = 5
+print type(c)
+
+# #use str() to convert int to a string
+d = b + str(c)
+print d
+
+e = "5"
+f = int(e) + c
+print f 
+
+
+
+
+
+
+
+
+
+#String Formatting
+# %s = for string
+name = "Emmett"
+text = 'Hi %s, you have 10 dollars in credits!' % name
+
+print text 
+
+name = "D'nae"
+credits = 50000
+text_2 = "Hi %s, you have %d dollars in credits!" % (name, credits)
+print text_2
+
+
+#list! aka Arrays
+names = ['Emmett', "D'nae", "Alex", "Braden", "Nate", "William", "Karen"]
+credits = [50, 10 , 3, 100 , 3, 5, 1]
+
+# print len(names)
+
+# #lists are 0 based, which means first item is list[0]
+# print names[0]
+# print names[2]
+
+
+
+for name in names:	
+	text_2 = "Hi %s, you have 100 dollars in credits!" % (name)
+	print text_2
+
+
+for a in range(6):
+	print 'a = ', a
+	text_2 = "Hi %s, you have %d dollars in credits!" % (names[a], credits[a])
+	print text_2
