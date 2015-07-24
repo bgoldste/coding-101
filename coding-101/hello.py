e1917916e85827a4dba3f5f9a6b601827e0a1191
Ben Goldstein <ben@Bens-MacBook-Pro.local>
2015-07-24 12:37:50 -0700

    add hello.py

---
+++ coding-101/hello.py
@@ -0,0 +1,14 @@
+#welcome to coding!
+
+#This is just a normal text file. All we have done is named it hello.py.
+#The .py extension makes it a python file, which means our text editor knows what the syntax should look like
+#and we can run it by pressing cmd + B!
+
+
+#lines that start with a hashtag/pound sign are commments.
+#this means that they don't do anything. 
+#it is also common to 'comment out' code that you do not want to run anymore
+
+
+#our first code
+print "Hello, world!"
