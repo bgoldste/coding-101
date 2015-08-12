cda7197210e8f8179506fecd661269128bc74b2f
Ben Goldstein <bengoldstein9@gmail.com>
2015-08-12 11:46:26 -0700

    Create lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,144 @@
+
+
+
+# #Strings
+# "ABC1234   "
+
+# #Adding Strings
+# "1" + "2" = "12"
+
+# "Hi" + "there" = "Hithere"
+
+
+#Variables store any type of value
+# x = "hi"
+# print x
+# print type(x)
+# #cmd + b to run any .py file in sublime
+
+
+# y = 5
+# print y
+# print type(y)
+
+
+# #print x + y ERROR!
+
+# #convert between types to add
+# print x + str(y) #prints "hi5"
+
+
+
+
+
+
+
+
+
+# patient1= "william"
+# patient2= "ben"
+# patient3= "karen"
+# patient4= "alex"
+# ...
+# patient4000000 = "asdad"
+
+
+
+
+
+#Lists 
+
+#an empty list 
+# patient_list = []
+
+#a list with one item
+# patient_list = ["william", "karen"]
+# print patient_list
+# print len(patient_list) #use len() to get length of a list
+
+# #lists are 0 based
+# #you can access items in a list like this
+# print patient_list[0] #IndexError: list index out of range
+
+
+
+
+
+
+# patient_list = ["william", "karen", "ben", "alex", "amanda"]
+# print len(patient_list)
+
+# print patient_list[3]
+
+
+
+
+
+
+
+
+#for loops.
+
+# number_list = [0, 1, 2, 3, 4]
+
+
+#for individual item IN larger list:
+	#do something with individual item
+
+# for number in number_list:
+# 	print number 
+
+
+
+# #built in function = range(N)
+# print range(5)
+
+
+# for number in range(5):
+# 	print number
+
+
+
+# patient_list = ["william", "karen", "ben", "alex", "amanda"]
+
+# for patient in patient_list:
+# 	print patient + " is a patient."
+
+
+
+
+
+
+
+
+#Fiz
+
+
+
+#if - else
+#use double equal ==
+
+a = 7
+
+print a == 5
+print a == 6
+
+if a == 5:
+	print "a equals 5"
+elif a ==6:
+	print "a equals 6"
+else:
+	print "a does not equal 5"
+
+
+
+
+
+
+
+
+
+
+
+
+
